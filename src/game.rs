b7873a7e00b59fed3c1d2f0d5a3906dfd39c1a9d
BarkerProoks <jonparkerbrooks@gmail.com>
2025-01-22 00:40:24 +0000

    progress

--- src/game.rs
+++ src/game.rs
@@ -3,20 +3,21 @@ use std::io::Read;
 
 use chess_engine::{ ChessMove, ChessBoard, ChessPieceColor };
 
+const INITIAL_CLIENTS: [Option<ChessClient>; 8] = [ None, None, None, None, None, None, None, None ];
+
 #[derive(Debug)]
 pub struct ChessPacket {
     data: [u8; 2], // move is a u16 but sockets send u8
     turn: u8 // turn number
 }
 
-#[derive(PartialEq)]
-pub struct ChessClient<'a> {
+pub struct ChessClient {
     board: ChessBoard,
-    client: &'a TcpStream
+    client: TcpStream
 }
 
-pub struct ChessServer<'a> {
-    clients: [Option<&'a ChessClient>; 8], // only 8 clients at a time
+pub struct ChessServer {
+    clients: [Option<ChessClient>; 8], // only 8 clients at a time
     service: TcpListener,
 }
 
@@ -38,35 +39,41 @@ impl ChessPacket {
 }
 
 impl ChessClient {
-    pub fn new(client: &mut TcpStream, color: ChessPieceColor) -> Self {
+    pub fn new(client: TcpStream, color: ChessPieceColor) -> Self {
         Self { client: client, board: ChessBoard::new(color) }
     }
 }
 
-impl ChessServer<'_> {
+impl ChessServer {
     pub fn new(host: &str, port: u16) -> Result<Self, String> {
         let service = TcpListener::bind(format!("{}:{}", host, port));
         match service {
-            Ok(service) => Ok( Self { service, clients: [None; 8]} ),
+            Ok(service) => Ok( Self { service, clients: INITIAL_CLIENTS } ),
             Err(_) => Err(format!("couldn't bind to address {host}:{port}"))
         }
     }
 
-    pub fn client_connected(&self, stream: &mut TcpStream) {
+    pub fn client_connected(&mut self, stream: TcpStream) {
         let index = self.clients.iter()
             .enumerate()
-            .find(|(_i, client)| **client == None)
-            .expect("too many clients").0;
+            .find(|(_i, client)| client.is_some())
+            .unwrap_or((0, &None)).0;
         
         println!("{}", index);
+    
+        self.clients[index] = Some(ChessClient::new(stream, ChessPieceColor::White));
     }
 
-    pub fn serve_forever(&self) {
+    pub fn serve_forever(&mut self) {
+        let mut streams: Vec<TcpStream> = vec![];
+
         for stream in self.service.incoming() {
             match stream {
-                Ok(mut stream) => self.client_connected(&mut stream),
+                Ok(stream) => streams.push(stream),
                 Err(error) => println!("error: {error}")
             }
+
+            println!("{:#?}", streams);
         }
     }
 }
